9abc55526d62332bf61fbb40ac64634ffd6dfcba
AnubhavPatil042 <2024.anubhav.patil@ves.ac.in>
2025-01-25 10:28:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+##Gemini Pro response
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page_num in range(len(reader.pages)):  # Fixed typo here (changed 'age' to 'page_num')
+        page = reader.pages[page_num]  # Corrected the page extraction
+        text += str(page.extract_text())  # Corrected method name to 'extract_text'
+    return text
+
+# Prompts for different actions
+input_prompt = """
+You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality. 
+Your task is to evaluate the resume against the provided job description. 
+Give me the percentage of match if the resume matches the job description. First, the output should come as a percentage, then list missing keywords, and finally provide your final thoughts.
+Assign the percentage matching based on jd and the missing keyword with high accuracy
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"Jd match" :"%","Missing keywords:[]","Profile Summary":""}}
+"""
+
+## streamlit app
+st.title("AtsProject")
+st.text("Resume Analyzer")
+jd = st.text_area("Paste the job description")
+uploaded_file = st.file_uploader("Upload Your resume", type="pdf", help="Please upload your pdf")
+
+submit = st.button("Analyze")
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_response(input_prompt.format(text=text, jd=jd))  # Passed the 'text' and 'jd' values
+        st.subheader(response)
